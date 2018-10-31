89e37cdec46b93de90498409af54aeb068886567
3A417141 <as05202005@gmail.com>
2018-10-31 21:12:53 +0800

    Exc-05  Using pattern to normalization

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('student/{student_no}', function ($student_no) {
     return "學號：".$student_no;
 });->while({'student_no'=>'s[0-9]{10}'});
@@ -22,7 +22,14 @@ Route::get('student/{student_no}/score', function ($student_no) {
 Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
     return "學號 ".$student_no." 的 ".$subject." 成績";
 });
+*/
+Route::pattern('student_no','3A[0-9]{6}');
+Route::pattern('subject','(Chinese|English|Math)');
+Route::get('student/{student_no}', function ($student_no) {
+    return "學號：".$student_no;
+});
 
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject = null) {
     return "學號 ".$student_no." 的 ".((is_null($subject))?"所有科目":$subject)." 成績";
 });
+
