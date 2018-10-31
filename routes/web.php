b09d3d931a5748be6d11d5d3f5fe6670ea79e39a
3A417141 <as05202005@gmail.com>
2018-10-31 21:17:42 +0800

    Exc-06  Group routes

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,16 @@ Route::get('student/{student_no}/score/{subject}', function ($student_no,$subjec
 */
 Route::pattern('student_no','3A[0-9]{6}');
 Route::pattern('subject','(Chinese|English|Math)');
+
+Route::group(['prefix'=>'student'],function(){
+	Route::get('{student_no}', function ($student_no) {
+		return "學號：".$student_no;
+	});
+	Route::get('{student_no}/score/{subject?}', function ($student_no,$subject = null) {
+		return "學號 ".$student_no." 的 ".((is_null($subject))?"所有科目":$subject)." 成績";
+	});
+});
+/*
 Route::get('student/{student_no}', function ($student_no) {
     return "學號：".$student_no;
 });
@@ -32,4 +42,4 @@ Route::get('student/{student_no}', function ($student_no) {
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject = null) {
     return "學號 ".$student_no." 的 ".((is_null($subject))?"所有科目":$subject)." 成績";
 });
-
+*/
