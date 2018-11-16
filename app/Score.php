e2ef10104b6538f6c08da3e50f7a434ec28cbea6
3A417141 <as05202005@gmail.com>
2018-11-16 12:21:13 +0800

    練習3 - 新增Student與Score model 並建立一對一關係

---
+++ app/Score.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Score extends Model
+{
+   protected $table = 'scores';
+    public function student(){
+        return $this->belongsTo(Student::class);
+}
