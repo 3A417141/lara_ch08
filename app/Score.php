6948c607bfea16bfe80916e26da473a515c5d995
3A417141 <as05202005@gmail.com>
2018-11-16 12:27:41 +0800

    練習3-3 - 建立兩個scope方法

--- app/Score.php
+++ app/Score.php
@@ -3,10 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\Student;
+
 
 class Score extends Model
 {
    protected $table = 'scores';
+   
     public function student(){
         return $this->belongsTo(Student::class);
+    }
+	 
+	 public function scopeOrderByTotal($query){
+        return $query->orderBy('score.total','DESC');
+    }
+    public function scopeOrderBySubject($query){
+        return $query->orderBy('score.chinese','DESC')
+                    ->orderBy('score.english','DESC')
+                    ->orderBy('score.math','DESC');
+    } 
 }
