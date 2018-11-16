7d3b6e9768b44ab7f8d9f4f6b2fdc0c4692e5ca1
3A417141 <as05202005@gmail.com>
2018-11-16 12:31:24 +0800

    練習4-1 - 修改BoardController 引用Score model & getIndex()方法

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Score;
 
 class BoardController extends Controller
 {
     public function getIndex()
     {
-        return View::make('board');
+        public function getIndex(){
+        $scores = Score::orderByTotal()
+                ->orderBySubject()->get();
+        $data = ['scores' => $scores];
+        return view('board', $data);
+    }
     }
 }
