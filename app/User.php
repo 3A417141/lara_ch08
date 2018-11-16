62f15a8c334cc7e1cc93602f96f783b6cee190c9
3A417141 <as05202005@gmail.com>
2018-11-16 12:24:44 +0800

    練習3-2 - 建立Student與User model 間一對一關係

--- app/User.php
+++ app/User.php
@@ -3,8 +3,8 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use \App\Student;
 
 class User extends Authenticatable
 {
@@ -27,4 +27,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+	
+	public function student(){
+        return $this->hasOne(Student::class);
+    }
 }
